7fd381ea2b11faf284ba894ec3a8aa775e96e04d
Ethan Liu <liuyuxuan1230@outlook.com>
2020-09-29 15:48:36 +0800

    hero on index.js

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,18 @@ import HoverButton from '../components/hover-button';
 
 export default function Home() {
   return (
-    <Hero>
-      <h1>Hi, I’m Ethan, Web Developer
-        and Web Designer
-      </h1>
-      <p>I design and build beautiful websites for businesses around the
-        globe. If you need a modern and powerful website, send me an email.
-        If we are a good fit, I will give you a time and cost estimate.</p>
-      <HoverButton/>
-    </Hero>
+    <>
+      <Hero>
+        <h1>Hi, I’m Ethan,
+          Web Developer and Web Designer
+        </h1>
+        <p>
+          I design and build beautiful websites for businesses around the
+          globe. If you need a modern and powerful website, send me an email.
+          If we are a good fit, I will give you a time and cost estimate.
+        </p>
+        <HoverButton>See my work</HoverButton>
+      </Hero>
+    </>
   );
 }
