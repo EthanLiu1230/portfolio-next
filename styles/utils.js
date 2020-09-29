8850f152dc319e23e8bea987481ed46c7313c504
Ethan Liu <liuyuxuan1230@outlook.com>
2020-09-29 14:49:37 +0800

    960px breakpoint for marginX,Y

--- styles/utils.js
+++ styles/utils.js
@@ -3,17 +3,17 @@ import { css } from 'styled-components';
 export const ContentMarginX = css`
   margin-left: 10%;
   margin-right: 10%;
-  @media (max-width: 768px) {
+  @media (max-width: 960px) {
     margin-left: 5%;
     margin-right: 5%;
   }
-`
+`;
 
 export const ContentMarginY = css`
   margin-top: 6rem;
   margin-bottom: 6rem;
-  @media (max-width: 768px) {
+  @media (max-width: 960px) {
     margin-top: 4rem;
     margin-bottom: 4rem;
   }
-`
+`;
