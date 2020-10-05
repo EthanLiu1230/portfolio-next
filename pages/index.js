08104294aa4444e780f298ab5c35cb5895e4a88c
Ethan Liu <liuyuxuan1230@outlook.com>
2020-10-05 19:34:33 +0800

    GridLayout on Homepage.

--- pages/index.js
+++ pages/index.js
@@ -58,6 +58,7 @@ export default function Home() {
         <CtaButton onClick={() => router.push('/first-page')}/>
       </Hero>
       <GridLayout>
+
         <LatestWork>
           <h3>latest work</h3>
           <h1>Chat App website</h1>
@@ -71,7 +72,6 @@ export default function Home() {
             responsive page in Webflow.</p>
           <CtaButton onClick={() => router.push('/first-page')}/>
         </LatestWork>
-
         <Thumbnail/>
 
         <LatestWork>
@@ -87,7 +87,24 @@ export default function Home() {
             responsive page in Webflow.</p>
           <CtaButton onClick={() => router.push('/first-page')}/>
         </LatestWork>
+        <Thumbnail/>
+
+        <h1>What I Do</h1>
+        <div>
+          <h2>Design</h2>
+          <p>I design beautiful and powerful websites for modern businesses. Any
+            business today needs a website that wins customers’ trust and helps
+            you do your business well. I make sure your website is up to that
+            standard.</p>
+          <h2>Development</h2>
+          <p>I build websites in Webflow where I can create responsive, powerful
+            and fully custom websites. Plus, Webflow has an incredibly simple
+            Content Editor for you and your team to edit website content quickly
+            and easily.</p>
+        </div>
+
       </GridLayout>
+
     </>
   );
 }
