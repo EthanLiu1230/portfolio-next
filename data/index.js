ad0cfeae747de74d2cb8923931cb16a203ae9b21
Ethan Liu <liuyuxuan1230@outlook.com>
2020-10-19 16:47:06 +0800

    getFooter from directus.

--- data/index.js
+++ data/index.js
@@ -1,10 +1,16 @@
-// data/ is for fetching data from strapi
 import axios from 'axios';
 
-export const baseUrl = 'http://127.0.0.1:1337';
-axios.defaults.baseURL = baseUrl;
+export const BASE_URL = 'http://localhost:8080/_example/items';
+axios.defaults.baseURL = BASE_URL;
 
-export const get = async (endpoint) => {
-  const res = await axios.get(endpoint);
-  return res.data;
+export const getData = async (endpoint) => {
+  const { data } = await axios.get(endpoint);
+  return data.data;
+};
+
+export const getFooter = async () => {
+  const res = await axios.get('/common');
+  const data = res.data.data;
+  const [{ footer }] = data;
+  return footer;
 };
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Hero from '../components/Hero';
 import React from 'react';
 import styled from 'styled-components';
 import CtaButton from '../components/CtaButton';
-import { get } from '../data';
+import { get, getFooter } from '../data';
 import { ContentMarginX, Panarama } from '../styles/utils';
 import Tag from '../components/Tag';
 import Thumbnail from '../components/Thumbnail';
@@ -11,6 +11,7 @@ import Footer from '../components/Footer';
 import { baseUrl } from '../data';
 
 import ReactMarkdown from 'react-markdown';
+import ReactHtmlParser from 'react-html-parser';
 
 const TagGroup = styled.div`
   display: inline-flex;
@@ -79,60 +80,55 @@ const WhatIDo = styled.section`
   }
 `;
 
-export default function Home ({ hero, projects, whatIDo, footer }) {
+export default function Home({ footer }) {
   const router = useRouter();
   return (
     <>
-      <Hero>
-        <ReactMarkdown source={hero}/>
-        <CtaButton onClick={() => router.push('/first-page')}/>
-      </Hero>
+      {/*<Hero>*/}
+      {/*  <ReactMarkdown source={hero}/>*/}
+      {/*  <CtaButton onClick={() => router.push('/first-page')}/>*/}
+      {/*</Hero>*/}
 
-      {
-        projects.map(project => (
-          <Project>
-            <LatestWork_A>
-              <Panarama>latest work</Panarama>
-              <h1>{project.title}</h1>
-              <TagGroup>
-                <Tag>Website Design</Tag>
-                <Tag>Web Development</Tag>
-                <Tag>Concept</Tag>
-              </TagGroup>
-            </LatestWork_A>
-            <ThumbnailWrap>
-              <Thumbnail image={`${baseUrl}${project.thumbnail.url}`}/>
-            </ThumbnailWrap>
+      {/*{*/}
+      {/*  projects.map(project => (*/}
+      {/*    <Project>*/}
+      {/*      <LatestWork_A>*/}
+      {/*        <Panarama>latest work</Panarama>*/}
+      {/*        <h1>{project.title}</h1>*/}
+      {/*        <TagGroup>*/}
+      {/*          <Tag>Website Design</Tag>*/}
+      {/*          <Tag>Web Development</Tag>*/}
+      {/*          <Tag>Concept</Tag>*/}
+      {/*        </TagGroup>*/}
+      {/*      </LatestWork_A>*/}
+      {/*      <ThumbnailWrap>*/}
+      {/*        <Thumbnail image={`${baseUrl}${project.thumbnail.url}`}/>*/}
+      {/*      </ThumbnailWrap>*/}
 
-            <LatestWork_B>
-              <ReactMarkdown source={project.description}/>
-              <CtaButton onClick={() => router.push('/first-page')}/>
-            </LatestWork_B>
-          </Project>))
-      }
+      {/*      <LatestWork_B>*/}
+      {/*        <ReactMarkdown source={project.description}/>*/}
+      {/*        <CtaButton onClick={() => router.push('/first-page')}/>*/}
+      {/*      </LatestWork_B>*/}
+      {/*    </Project>))*/}
+      {/*}*/}
 
-      <WhatIDo>
-        <h1>What I <strong>Do</strong></h1>
-        <div>
-          <ReactMarkdown source={whatIDo}/>
-        </div>
-      </WhatIDo>
+      {/*<WhatIDo>*/}
+      {/*  <h1>What I <strong>Do</strong></h1>*/}
+      {/*  <div>*/}
+      {/*    <ReactMarkdown source={whatIDo}/>*/}
+      {/*  </div>*/}
+      {/*</WhatIDo>*/}
       <Footer>
-        <ReactMarkdown source={footer}/>
+        {ReactHtmlParser(footer)}
       </Footer>
     </>
   );
 }
 
-export async function getStaticProps () {
-  const { hero, whatIDo } = await get(`/homepage`);
-  const projects = await get(`/projects`);
-  const { footer } = await get(`/common`);
+export async function getStaticProps() {
+  const footer = await getFooter();
   return {
     props: {
-      hero,
-      whatIDo,
-      projects,
       footer,
     },
   };
